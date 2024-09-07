fd0ed060ba4d9100cb2d74722b8cd571d6e36482
saiyan <hohuuminh98@gmail.com>
2024-09-07 20:29:24 +0700

    update code

---
+++ script.js
@@ -0,0 +1,54 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Ẩn tất cả nội dung tab
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Xóa lớp active cho tất cả các tab
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Hiển thị tab đã chọn và thêm lớp active cho nút tab đó
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    // Tải nội dung cho Tab 2
+    if (tabName === 'Tab2') {
+        loadTabContent();
+    }
+}
+
+function loadTabContent() {
+    fetch('tutorial.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('Tab2').innerHTML = data;
+            // Gọi hàm để thay đổi nội dung h2 sau khi đã tải xong
+            updateH2InTab2();
+        })
+        .catch(error => console.error('Error loading tutorial content:', error));
+    
+    fetch('montage.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('Tab3').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading montage content:', error));
+}
+
+function updateH2InTab2() {
+    const h2 = document.querySelectorAll("#Tab2 h2");
+    h2.forEach((heading, index) => {
+        heading.textContent = `${index + 1}. ` + heading.textContent;
+    });
+}
+
+// Mở tab mặc định khi tải trang
+window.onload = function() {
+    document.getElementsByClassName("tablinks")[0].click();
+};
